f98ea62efa64de185df61353897538a7c702284a
Ivan Perez Avellaneda <80877217+iperezav@users.noreply.github.com>
2024-07-13 17:22:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,35 @@
 
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0'
+DESCRIPTION = 'Chen-Fliess series computation'
+LONG_DESCRIPTION = 'A package that allows to simulate the output of a control system by means of the Chen-Fliess series.'
+
+# Setting up
+setup(
+    name="CFS",
+    version=VERSION,
+    author="Ivan Perez Avellaneda",
+    author_email="<iperezave@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy', 'sympy'],
+    keywords=['Chen-Fliess series', 'nonlinear system', 'input-output system', 'ODEs', 'control system', 'system theory', 'python'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
