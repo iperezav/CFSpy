bd1f3df862a207e30b2a38c23cd9af13ad22fbc3
Ivan Perez Avellaneda <iperezavellaneda@gmail.com>
2025-08-26 00:21:06 -0400

    deleted a quotation mark in iter_int

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '1.0.13'
+VERSION = '1.2.1'
 DESCRIPTION = 'Chen-Fliess series computation'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
